ecb430e929a6548d70fe79fbe59299adf456ee5b
negar.kamyar <negar72vk@gmail.com>
2020-07-26 22:15:04 +1000

    introduce CardHeader and Title

---
+++ src/stories/CardHeader.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CardHeader from "../components/CardHeaderComponent";
+
+export default {
+    title: "Card Header",
+    component: CardHeader,
+};
+export const onlyTitle= () => <CardHeader text="salammm dodo"></CardHeader>;
+
---
+++ src/styles/typography.js
@@ -0,0 +1,20 @@
+const smallTextSize = 14;
+const mediumTextSize = 16;
+const largeTextSize = 18;
+const regularWeight ='regular';
+const mediumWeight ='bold';
+const textStyles = {
+    smallReg: {
+        fontSize: smallTextSize,
+        fontWeight:regularWeight,
+    },
+    mediumReg: {
+        fontSize: mediumTextSize,
+        fontWeight: regularWeight,
+    },
+    largeBold: {
+        fontSize: largeTextSize,
+        fontWeight: mediumWeight,
+    },
+}
+export default textStyles
